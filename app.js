26fc73cb77dc957b557bfa98202735f050a6d7d6
Кики <57362643+kikibyk@users.noreply.github.com>
2022-11-05 23:07:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-let config = require('./config.json');
-const rq = require('prequest');
+const { access_token, post } = require('./config.json'),
+    https = require('https'),
+    querystring = require('node:querystring'),
+    crypto = require('crypto'),
+    pattern = ["❤", "💜", "💛", "💚", "💙", "🖤"];
 
-const hearts = ["❤", "💜", "💛", "💚", "💙", "🖤"];
+setInterval(async () => {
+    https.get("https://api.vk.com/method/wall.createComment?" +
+                      querystring.stringify(Object.assign({ access_token, text: pattern[crypto.randomInt(pattern.length)], v: "5.83" }, post)), res => {
+        res.setEncoding('utf8');
+        res.once("data", data => {
+            const { response, error } = JSON.parse(data);
 
-setInterval(() => {
-	config.tokens.map(x => {
-		rq(`https://api.vk.com/method/wall.createComment?owner_id=${config.owner_id}&post_id=${config.post_id}&access_token=${x}&v=5.83&text=${encodeURI(hearts[Math.floor(Math.random() * hearts.length)])}`).then(res => {
-			if(!res['response']) console.log(`\x1b[31mКомментарий не был оставлен | ${x} (${res['error'].error_msg})\x1b[0m`);
-			else console.log(`\x1b[32mКомментарий оставлен.\x1b[0m`);
-		});
-	});
-}, config.cd);
+            if(error) console.log(`\x1b[31m> \x1b[0mКомментарий не был оставлен \x1b[31m| \x1b[0m(${error.error_msg})\x1b[0m`);
+            else console.log(`\x1b[36m> \x1b[0mКомментарий оставлен \x1b[36m| \x1b[0m${response.comment_id}\x1b[0m`);
+        });
+    })
+}, 15000);
